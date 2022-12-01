a733b8dac5cc93f8aa1f51d60edcb3550bf60a91
manijehshirzadeh <manijeh.shirzadeh63@gmail.com>
2022-12-01 13:13:05 +1100

    Add path to go to the tasks page

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 // TASK 6
-import { TaskManager } from './TaskManager.js';
-import { validateForm } from './create.js';
+import { TaskManager } from "./TaskManager.js";
+import { validateForm } from "./create.js";
 
-const STORAGE_KEY_PREFIX = 'TASK_LIST_WITH_LOCAL_STORAGE';
+const STORAGE_KEY_PREFIX = "TASK_LIST_WITH_LOCAL_STORAGE";
 const STORAGE_KEY = `${STORAGE_KEY_PREFIX}-GROUP1`;
 
 // Input function
 const form = document.forms[0];
-const taskName = document.getElementById('task');
-const taskAssignee = document.getElementById('assignee');
-const taskDescription = document.getElementById('description');
-const taskDueDate = document.getElementById('due-date');
-const taskStatus = document.getElementById('status');
-const errorText = document.querySelectorAll('small.form-text.text-muted');
+const taskName = document.getElementById("task");
+const taskAssignee = document.getElementById("assignee");
+const taskDescription = document.getElementById("description");
+const taskDueDate = document.getElementById("due-date");
+const taskStatus = document.getElementById("status");
+const errorText = document.querySelectorAll("small.form-text.text-muted");
 
-const successMessage = document.createElement('span');
-successMessage.innerText = 'Task added success!';
-successMessage.className = 'text-success';
-successMessage.style.marginLeft = '18px';
+const successMessage = document.createElement("span");
+successMessage.innerText = "Task added success!";
+successMessage.className = "text-success";
+successMessage.style.marginLeft = "18px";
 
 function clearSuccessMessage() {
-	successMessage.remove();
+  successMessage.remove();
 }
 
 setInterval(() => {
-	clearSuccessMessage();
-	errorText.forEach((error) => {
-		error.innerHTML = '';
-	});
+  clearSuccessMessage();
+  errorText.forEach((error) => {
+    error.innerHTML = "";
+  });
 }, 5000);
 
 clearInterval(clearSuccessMessage());
@@ -36,19 +36,20 @@ const taskManager = new TaskManager(STORAGE_KEY);
 
 // Save form to local storage
 form.onsubmit = (e) => {
-	e.preventDefault();
-	const task = {
-		id: Date.now(),
-		name: taskName.value,
-		description: taskDescription.value,
-		assignedTo: taskAssignee.value,
-		dueDate: taskDueDate.value,
-		status: taskStatus.value,
-	};
-	const isValidated = validateForm();
-	if (isValidated) {
-		taskManager.addTask(task);
-		const formEnd = form.lastElementChild;
-		form.insertBefore(successMessage, formEnd);
-	}
+  e.preventDefault();
+  const task = {
+    id: Date.now(),
+    name: taskName.value,
+    description: taskDescription.value,
+    assignedTo: taskAssignee.value,
+    dueDate: taskDueDate.value,
+    status: taskStatus.value,
+  };
+  const isValidated = validateForm();
+  if (isValidated) {
+    taskManager.addTask(task);
+    const formEnd = form.lastElementChild;
+    form.insertBefore(successMessage, formEnd);
+    window.location.href = "tasks.html";
+  }
 };
